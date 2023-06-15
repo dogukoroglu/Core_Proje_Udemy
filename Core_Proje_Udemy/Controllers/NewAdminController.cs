c99ce564546b5075077c0d903041b07566c646e8
korog <korog@LAPTOP-GPMKKC5N>
2023-06-15 22:53:13 +0300

    Yeni admin teması eklendi

---
+++ Core_Proje_Udemy/Controllers/NewAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje_Udemy.Controllers
+{
+    public class NewAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
