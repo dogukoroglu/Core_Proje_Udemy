83e43c79d8f5ef3e12febc961897146ffb5c0964
korog <korog@LAPTOP-GPMKKC5N>
2023-06-20 11:24:28 +0300

    Mesaj işlemleri için katmanlara ekleme yapıldı.

---
+++ BusinessLayer/Concrete/WritterMessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class WritterMessageManager : IWritterMessageService
+	{
+		IWritterMessageDal _writterMessageDal;
+
+		public WritterMessageManager(IWritterMessageDal writterMessageDal)
+		{
+			_writterMessageDal = writterMessageDal;
+		}
+
+		public void TAdd(WritterMessage t)
+		{
+			_writterMessageDal.Insert(t);
+		}
+
+		public void TDelete(WritterMessage t)
+		{
+			_writterMessageDal.Delete(t);
+		}
+
+		public WritterMessage TGetByID(int id)
+		{
+			return _writterMessageDal.GetByID(id);
+		}
+
+		public List<WritterMessage> TGetList()
+		{
+			return _writterMessageDal.GetList();
+		}
+
+		public void TUpdate(WritterMessage t)
+		{
+			_writterMessageDal.Update(t);
+		}
+	}
+}
