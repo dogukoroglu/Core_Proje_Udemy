45ce9e4bdd53e1d58fc8935bf2eb293383f4de32
korog <korog@LAPTOP-GPMKKC5N>
2023-06-24 00:16:10 +0300

    Sosyal medya alanı dinamik hale getirildi.

---
+++ Core_Proje_Udemy/Controllers/TestimonialController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje_Udemy.Controllers
+{
+	public class TestimonialController : Controller
+	{
+		TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+		public IActionResult Index()
+		{
+			var values = testimonialManager.TGetList();
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult AddTestimonial()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult AddTestimonial(Testimonial testimonial)
+		{
+			testimonialManager.TAdd(testimonial);
+			return RedirectToAction("Index", "Testimonial");
+		}
+
+		public IActionResult DeleteTestimonial(int id)
+		{
+			var value = testimonialManager.TGetByID(id);
+			testimonialManager.TDelete(value);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateTestimonial(int id)
+		{
+			var value = testimonialManager.TGetByID(id);
+			return View(value);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateTestimonial(Testimonial testimonial)
+		{
+			testimonialManager.TUpdate(testimonial); 
+			return RedirectToAction("Index","Testimonial");
+		}
+	}
+}
